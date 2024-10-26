75bca0120ba859318fb51c73404d7312e4b725a4
xLordkayx <westwalley061@gmail.com>
2024-10-26 18:11:06 +0100

    Add files via upload

---
+++ lazackcmds/greetings.js
@@ -0,0 +1,24 @@
+export async function all(m) {
+  // when someone sends you hello message
+  if (
+    (m.mtype === 'hellomessage' ||
+      m.text.startsWith('silva')) &&
+    !m.isBaileys &&
+    !m.isGroup
+ /* ) {
+    this.sendMessage(
+      m.chat,
+      {
+        text: `Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim(),
+      },
+      { quoted: m }*/
+    ) {
+    this.sendButton(m.chat, `*WELCOME THIS IS REDHOOD MD BOT*      
+    Hello 🤟🏻😇 @${m.sender.split('@')[0]} 
+    THANK YOU FOR MESSAGING ME I WILL RESPOND IF I DONT KNOW I'M OFFLINE😇
+  `.trim(), igfg, null, [['OWNER HELP', '.grp'],['BOT SCRIPT', '.repo']] , m, { mentions: [m.sender] })
+    m.react('💕')
+  }
+
+  return !0
+}
